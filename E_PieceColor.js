d86e9027a893fd44208351d3dff082c57336f727
David <jdiazga@unal.edu.co>
2023-09-21 12:24:02 -0500

    Initial commit: Add PieceColor and PieceType enums

---
+++ E_PieceColor.js
@@ -0,0 +1,7 @@
+const E_PieceColor = Object.freeze({
+    Black: Symbol("White"),
+    White: Symbol("Black"),
+    None: Symbol("Black")
+})
+
+
---
+++ E_PieceType.js
@@ -0,0 +1,11 @@
+const E_PieceType = Object.freeze({
+    King: Symbol("King"),
+    Queen: Symbol("Queen"),
+    Bishop: Symbol("Bishop"),
+    Rook: Symbol("Rook"),
+    Knight: Symbol("Knight"),
+    Pawn: Symbol("Pawn"),
+    None: Symbol("None")
+})
+
+
