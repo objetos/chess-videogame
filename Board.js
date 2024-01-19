bab7df8760757f9990d1919b10ea7678f3289812
David <jdiazga@unal.edu.co>
2024-01-18 20:49:58 -0500

    - Add documentation of (most) methods - Construct Board with FEN - Add method for move generation

--- Board.js
+++ Board.js
@@ -1,38 +1,55 @@
-//******  CLASS PROLOG, ASSERT FLOATING INPUTS
+//******  CLASS PROLOG, ASSERT FLOATING INPUTS, DOCUMENT CONSTRUCTOR, REMOVE MAGIC NUMBERS
 class Board {
     static #FIRST_FILE = 0x0101010101010101n;
     static #FIRST_RANK = 0xFFn;
     #piecesDictionary = {};
     #piecesMatrix = [];
+    #pieces = [];
 
-    static GetFile(inputFileNumber) {
-        console.assert(typeof inputFileNumber === "number", "File is of incorrect type");
-        console.assert(inputFileNumber >= 1 && inputFileNumber <= 8, "File " + inputFileNumber + " is out of bounds.");
+    /**
+     * Gives a bitboard with a single file.
+     * @param {number} fileNumber Number of the file, going from 1 to 8, where 1 is the leftmost column of the board.
+     * @returns {BigInt} Bitboard that contains the specified file.
+     */
+    static GetFile(fileNumber) {
+        console.assert(typeof fileNumber === "number", "File is of incorrect type");
+        console.assert(fileNumber >= 1 && fileNumber <= 8, "File " + fileNumber + " is out of bounds.");
 
         let fileBitboard = this.#FIRST_FILE;
         //Move first file n positions
-        fileBitboard = fileBitboard << BigInt(8 - inputFileNumber);
+        fileBitboard = fileBitboard << BigInt(8 - fileNumber);
         return fileBitboard;
     }
 
-    static GetRank(inputRankNumber) {
-        console.assert(typeof inputRankNumber === "number", "Rank is of incorrect type");
-        console.assert(inputRankNumber >= 1 && inputRankNumber <= 8, "Rank " + inputRankNumber + " is out of bounds.");
+    /**
+     * Gives a bitboard with a single file.
+     * @param {number} rankNumber Number of the rank, going from 1 to 8, where 1 is the bottom row of the board.
+     * @returns {BigInt} Bitboard that contains the specified rank.
+     */
+    static GetRank(rankNumber) {
+        console.assert(typeof rankNumber === "number", "Rank is of incorrect type");
+        console.assert(rankNumber >= 1 && rankNumber <= 8, "Rank " + rankNumber + " is out of bounds.");
 
         let rankBitboard = this.#FIRST_RANK;
         //Move first rank n positions
-        rankBitboard = rankBitboard << BigInt((inputRankNumber - 1) * 8);
+        rankBitboard = rankBitboard << BigInt((rankNumber - 1) * 8);
         return rankBitboard;
     }
 
+    /**
+     * Gives a bitboard with a single diagonal.
+     * @param {number} inputDiagonalNumber Number of the diagonal, going from 1 to 15, where 1 is the bottom right diagonal 
+     * and 15 the top left diagonal of the board.
+     * @returns {BigInt} Bitboard that contains the specified diagonal.
+     */
     static GetDiagonal(inputDiagonalNumber) {
         console.assert(typeof inputDiagonalNumber === "number", "Diagonal is of incorrect type");
         console.assert(inputDiagonalNumber >= 1 && inputDiagonalNumber <= 15, "Diagonal " + inputDiagonalNumber + " is out of bounds.");
 
         // Calculate for up to the eight diagonal
         let diagonalNumber = inputDiagonalNumber;
-        if (8 < inputDiagonalNumber) {
-            diagonalNumber = 8 - (inputDiagonalNumber % 8);
+        if (8 < diagonalNumber) {
+            diagonalNumber = 8 - (diagonalNumber % 8);
         }
 
         //Build the diagonal procedurally
@@ -49,6 +66,12 @@ class Board {
         return diagonalBitboard;
     }
 
+    /**
+     * Gives a bitboard with a single antidiagonal.
+     * @param {number} inputAntiDiagonalNumber Number of the antidiagonal, going from 1 to 15, where 1 is the bottom left antidiagonal 
+     * and 15 the top right antidiagonal of the board.
+     * @returns {BigInt} Bitboard that contains the specified antidiagonal.
+     */
     static GetAntiDiagonal(inputAntiDiagonalNumber) {
         console.assert(typeof inputAntiDiagonalNumber === "number", "AntiDiagonal is of incorrect type");
         console.assert(inputAntiDiagonalNumber >= 1 && inputAntiDiagonalNumber <= 15, "AntiDiagonal " + inputAntiDiagonalNumber + " is out of bounds.");
@@ -60,7 +83,12 @@ class Board {
         return antiDiagonalBitboard;
     }
 
-    //(REF)Taken from: https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating
+    /**
+     * Flips a bitboard along the 8th diagonal (middle diagonal).
+     * Taken from: https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating
+     * @param {BigInt} bitboard 
+     * @returns {BigInt} Flipped bitboard
+     */
     static FlipDiagonally(bitboard) {
         console.assert(typeof bitboard === "bigint", "Incorrect type");
 
@@ -79,7 +107,12 @@ class Board {
         return bitboard;
     }
 
-    //(REF)Taken from: https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating
+    /**
+     * Flips a bitboard along the 8th antidiagonal (middle antidiagonal).
+     * Taken from: https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating
+     * @param {BigInt} bitboard 
+     * @returns {BigInt} Flipped bitboard
+     */
     static FlipAntiDiagonally(bitboard) {
         console.assert(typeof bitboard === "bigint", "Incorrect type");
 
@@ -98,7 +131,12 @@ class Board {
         return bitboard;
     }
 
-    //(REF)Taken from: https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating
+    /**
+     * Mirrors a bitboard along a vertical line.
+     * Taken from: https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating
+     * @param {BigInt} bitboard 
+     * @returns {BigInt} Mirrored bitboard 
+     */
     static MirrorHorizontally(bitboard) {
         console.assert(typeof bitboard === "bigint", "Incorrect type");
 
@@ -113,110 +151,8 @@ class Board {
         return bitboard;
     }
 
-    constructor(inputPieceMatrix) {
-        this.#CreatePieces(inputPieceMatrix);
-    }
-
-    GetPiecesOfType(pieceType) {
-
-        console.assert(Object.values(E_PieceType).includes(pieceType), "Piece type not defined");
-
-        //create a empty board
-        let pieceTypeBitboard = 0n;
-
-        //find all pieces of given type
-        let piecesOfType = new Array();
-        for (let pieceColor of Object.values(E_PieceColor)) {
-            piecesOfType = piecesOfType.concat(this.#piecesDictionary[pieceColor][pieceType])
-        }
-
-        //for each piece of given type    
-        for (let piece of piecesOfType) {
-            //get location in board
-            let position = piece.GetPosition();
-            //add it to board
-            pieceTypeBitboard = pieceTypeBitboard | position;
-        }
-
-        return pieceTypeBitboard;
-    }
-
-    GetPiecesOfColor(pieceColor) {
-
-        console.assert(Object.values(E_PieceColor).includes(pieceColor), "Piece color not defined");
-
-        //create a empty board
-        let pieceColorBitboard = 0n;
-
-        //find all pieces of given color
-        let piecesOfColor = new Array();
-        for (let pieceType of Object.values(E_PieceType)) {
-            piecesOfColor = piecesOfColor.concat(this.#piecesDictionary[pieceColor][pieceType])
-        }
-
-        //for each piece of given color    
-        for (let piece of piecesOfColor) {
-            //get location in board
-            let position = piece.GetPosition();
-            //add it to board
-            pieceColorBitboard = pieceColorBitboard | position;
-        }
-
-        return pieceColorBitboard;
-    }
-
-    GetOccupied() {
-        //test: square by square check
-
-        //get board occupied by white pieces
-        let whitePieces = this.GetPiecesOfColor(E_PieceColor.White);
-        //get board occupied by black pieces
-        let blackPieces = this.GetPiecesOfColor(E_PieceColor.Black);
-        //join white board and black board
-        let occupied = whitePieces | blackPieces;
-        return occupied;
-    }
-
-    GetEmptySpaces() {
-        //test: square by square check
-        //get board occupied by pieces
-        let occupied = this.GetOccupied();
-        //reverse board to obtain empty spaces
-        let empty = ~occupied;
-
-        return empty;
-    }
-
-
-    ApplyMove(move) {
-
-
-    }
-
-    Print() {
-        let string = "";
-        for (let rankIndex = 0; rankIndex < this.#piecesMatrix.length; rankIndex++) {
-            for (let fileIndex = 0; fileIndex < this.#piecesMatrix[rankIndex].length; fileIndex++) {
-
-                let piece = this.#piecesMatrix[rankIndex][fileIndex];
-
-                if (piece === undefined) {
-                    string += " 00";
-                } else {
-                    string += " " + piece.ToString();
-                }
-
-                if (((fileIndex + 1) % 8) === 0) {
-                    string += "\n";
-                }
-            }
-        }
-        console.log(string);
-    }
-
-    #CreatePieces(inputPieceMatrix) {
-        console.assert(Array.isArray(inputPieceMatrix), "Input is not a matrix");
-        console.assert(inputPieceMatrix.length == 8, "Input matrix is not 8x8");
+    constructor(inputFen) {
+        let chessBoard = new Quadrille(inputFen);
 
         //initialize dictionary of pieces
         this.#piecesDictionary = {};
@@ -227,25 +163,14 @@ class Board {
             }
         }
 
-        //initialize board
-        this.#piecesMatrix = [];
-        for (let i = 0; i < 8; i++) {
-            this.#piecesMatrix.push(new Array(8));
-        }
-
         //for each square
-        for (let rankIndex = 0; rankIndex < inputPieceMatrix.length; rankIndex++) {
-            console.assert(Array.isArray(inputPieceMatrix[rankIndex]), "Input is not a matrix");
-            console.assert(inputPieceMatrix[rankIndex], "Input matrix is not 8x8");
-
-            for (let fileIndex = 0; fileIndex < inputPieceMatrix[rankIndex].length; fileIndex++) {
+        for (let rankIndex = 0; rankIndex < 8; rankIndex++) {
+            for (let fileIndex = 0; fileIndex < 8; fileIndex++) {
 
                 //if there's a piece
-                let piece = inputPieceMatrix[rankIndex][fileIndex];
-                if (piece === undefined) continue;
-
-                console.assert(!(piece.type === undefined), "Piece has no type property");
-                console.assert(!(piece.color === undefined), "Piece has no color property");
+                let pieceSymbol = chessBoard.read(rankIndex, fileIndex);
+                if (pieceSymbol === null) continue;
+                let piece = Quadrille.chessKeys[pieceSymbol];
 
                 //create a piece
                 let rank = 8 - rankIndex;
@@ -253,40 +178,222 @@ class Board {
                 let pieceObject = this.#CreatePiece(piece, rank, file);
 
                 //categorize it by color and type
-                this.#piecesDictionary[piece.color][piece.type].push(pieceObject);
+                this.#piecesDictionary[pieceObject.color][pieceObject.GetType()].push(pieceObject);
 
-                //add it to the board
-                this.#piecesMatrix[rankIndex][fileIndex] = pieceObject;
+                //add it to pieces array
+                this.#pieces.push(pieceObject);
             }
         }
+        this.#piecesMatrix = chessBoard;
+    }
+
+    /**
+     * 
+     * @param {Board} board 
+     * @returns {Move[]} Array of legal moves
+     */
+    GetLegalMoves() {
+        //secrets: how are moves calculated, transformation from bitboard to moves
+        //input:board with pieces
+        //output:array of moves with start and end position
+        //test: visual test with individual pieces, premade arrays of moves, 
+        //errors: null board --> crash,
+
+        let legalMoves = new Array();
+
+        this.#pieces.forEach(piece => {
+            //get board with permitted moves
+            let pieceMoves = piece.GetMoves(this);
+            let testBit = 1n;
+            //for each square
+            for (let index = 0; index < 64; index++) {
+                //if square is permitted
+                let squarePermitted = (pieceMoves & testBit) > 0n;
+                if (squarePermitted) {
+                    //create move from piece position to square
+                    let endRank = Math.floor((index) / 8) + 1;
+                    let endFile = 8 - (index % 8);
+                    let newMove = new Move(piece.rank, piece.file, endRank, endFile);
+                    //add move to array
+                    legalMoves.push(newMove);
+                }
+                //continue to next square
+                testBit = testBit << 1n;
+            }
+        });
+
+        return legalMoves;
+    }
+
+
+    /** 
+     * @param {E_PieceType} pieceType 
+     * @returns {BigInt} Bitboard that contains pieces of given type.
+     */
+    GetPiecesOfType(pieceType) {
+
+        console.assert(Object.values(E_PieceType).includes(pieceType), "Piece type not defined");
+
+        //create a empty board
+        let pieceTypeBitboard = 0n;
+
+        //find all pieces of given type
+        let piecesOfType = new Array();
+        for (let pieceColor of Object.values(E_PieceColor)) {
+            piecesOfType = piecesOfType.concat(this.#piecesDictionary[pieceColor][pieceType]);
+        }
+
+        //for each piece of given type    
+        for (let piece of piecesOfType) {
+            //get location in board
+            let position = piece.position;
+            //add it to board
+            pieceTypeBitboard = pieceTypeBitboard | position;
+        }
+
+        return pieceTypeBitboard;
+    }
+
+    /**
+     * @param {E_PieceColor} pieceColor 
+     * @returns Bitboard that contains pieces of given color.
+     */
+    GetPiecesOfColor(pieceColor) {
+
+        console.assert(Object.values(E_PieceColor).includes(pieceColor), "Piece color not defined");
+
+        //create a empty board
+        let pieceColorBitboard = 0n;
+
+        //find all pieces of given color
+        let piecesOfColor = new Array();
+        for (let pieceType of Object.values(E_PieceType)) {
+            piecesOfColor = piecesOfColor.concat(this.#piecesDictionary[pieceColor][pieceType]);
+        }
+
+        //for each piece of given color    
+        for (let piece of piecesOfColor) {
+            //get location in board
+            let position = piece.position;
+            //add it to board
+            pieceColorBitboard = pieceColorBitboard | position;
+        }
+
+        return pieceColorBitboard;
+    }
+
+    /**
+     * @returns Bitboard that contains all spaces occupied by a piece.
+     */
+    GetOccupied() {
+        //get board occupied by white pieces
+        let whitePieces = this.GetPiecesOfColor(E_PieceColor.White);
+        //get board occupied by black pieces
+        let blackPieces = this.GetPiecesOfColor(E_PieceColor.Black);
+        //join white board and black board
+        let occupied = whitePieces | blackPieces;
+        return occupied;
+    }
+
+
+    /**
+     * @returns Bitboard that contains all spaces not occupied by a piece.
+     */
+    GetEmptySpaces() {
+        //test: square by square check
+        //get board occupied by pieces
+        let occupied = this.GetOccupied();
+        //reverse board to obtain empty spaces
+        let empty = ~occupied;
+
+        return empty;
+    }
+
+    /**
+     * 
+     * @returns FEN representation of board 
+     */
+
+    GetFen() {
+        return this.#piecesMatrix.toFEN();
+    }
+
+    /**
+     * 
+     * @param {Move} move Applies move to board
+     */
+    ApplyMove(move) {
+
+
+    }
+
+    /**
+     * Prints 8x8 chess board in the console showing pieces' position and type. 
+     * Lowercase letters are for black pieces. Uppercase letters are for white pieces.
+     * W = White. B = Black. P = Pawn. R = Rook. N = Knight. B = Bishop. Q = Queen. K = King. # = Empty.
+     */
+    Print() {
+        let string = "";
+        for (let rankIndex = 0; rankIndex < 8; rankIndex++) {
+            for (let fileIndex = 0; fileIndex < 8; fileIndex++) {
+
+                let piece = this.#piecesMatrix.read(rankIndex, fileIndex);
+
+                if (piece === null) {
+                    string += " #";
+                } else {
+                    string += " " + Quadrille.chessKeys[piece];
+                }
+
+                if (((fileIndex + 1) % 8) === 0) {
+                    string += "\n";
+                }
+            }
+        }
+        console.log(string);
     }
 
     #CreatePiece(piece, rank, file) {
         let pieceObject = null;
-        switch (piece.type) {
-            case E_PieceType.King:
-                pieceObject = new King(piece.color, rank, file);
+        switch (piece) {
+            case 'K':
+                pieceObject = new King(E_PieceColor.White, rank, file);
                 break;
-            case E_PieceType.Queen:
-                pieceObject = new Queen(piece.color, rank, file);
+            case 'Q':
+                pieceObject = new Queen(E_PieceColor.White, rank, file);
                 break;
-            case E_PieceType.Bishop:
-                pieceObject = new Bishop(piece.color, rank, file);
+            case 'B':
+                pieceObject = new Bishop(E_PieceColor.White, rank, file);
                 break;
-            case E_PieceType.Rook:
-                pieceObject = new Rook(piece.color, rank, file);
+            case 'R':
+                pieceObject = new Rook(E_PieceColor.White, rank, file);
                 break;
-            case E_PieceType.Knight:
-                pieceObject = new Knight(piece.color, rank, file);
+            case 'N':
+                pieceObject = new Knight(E_PieceColor.White, rank, file);
                 break;
-            case E_PieceType.Pawn:
-                pieceObject = new Pawn(piece.color, rank, file);
+            case 'P':
+                pieceObject = new Pawn(E_PieceColor.White, rank, file);
                 break;
-            case E_PieceType.None:
-                console.error("No type set to piece");
+            case 'k':
+                pieceObject = new King(E_PieceColor.Black, rank, file);
+                break;
+            case 'q':
+                pieceObject = new Queen(E_PieceColor.Black, rank, file);
+                break;
+            case 'b':
+                pieceObject = new Bishop(E_PieceColor.Black, rank, file);
+                break;
+            case 'r':
+                pieceObject = new Rook(E_PieceColor.Black, rank, file);
+                break;
+            case 'n':
+                pieceObject = new Knight(E_PieceColor.Black, rank, file);
+                break;
+            case 'p':
+                pieceObject = new Pawn(E_PieceColor.Black, rank, file);
                 break;
             default:
-                console.error("Incorrect piece type:" + piece.type);
+                throw new Error("Incorrect piece type:" + piece);
                 break;
         }
         return pieceObject;
