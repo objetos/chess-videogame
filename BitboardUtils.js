f4aa3f6d7e8af331c65fda9ed6540d572d0102c8
David <jdiazga@unal.edu.co>
2024-02-25 09:29:09 -0500

    Add functionality: - Calculate bitboard with a ray from start square to destination sqaure in board - Convert given rank and file into a bitboard - Calculate number of  bits of a bitboard - Hyberbola Quintessence algorithm

--- BitboardUtils.js
+++ BitboardUtils.js
@@ -1,3 +1,72 @@
+function RankFileToBitboard(rank, file) { //****** assertions, document
+    //move to rank
+    let bitboard = 1n << BigInt(8 - file);
+    //move to file
+    bitboard = bitboard << BigInt((rank - 1) * 8);
+    return bitboard;
+}
+
+function GetRay(startRank, startFile, endRank, endFile, includeStart = true, includeEnd = true) {
+    let start = RankFileToBitboard(startRank, startFile);
+    let end = RankFileToBitboard(endRank, endFile);
+
+    if (startRank === endRank && startFile === endFile) {
+        if (includeStart || includeEnd) return start;
+        else return 0n;
+    }
+
+    let rankDiff = startRank - endRank;
+    let fileDiff = startFile - endFile;
+    let isPositiveRay = false;
+    let mask = 0n;
+
+    if (startRank === endRank) {
+        mask = Board.GetRank(startRank);
+        isPositiveRay = startFile > endFile;
+    } else if (startFile === endFile) {
+        mask = Board.GetFile(startFile);
+        isPositiveRay = startRank < endRank;
+    } else if (Math.abs(rankDiff) === Math.abs(fileDiff)) {
+        let isDiagonal = Math.sign(rankDiff) === Math.sign(fileDiff);
+        if (isDiagonal) {
+            mask = Board.GetDiagonal(startRank, startFile);
+            isPositiveRay = rankDiff < 0 && fileDiff < 0;
+        } else {
+            mask = Board.GetAntiDiagonal(startRank, startFile);
+            isPositiveRay = 0 < rankDiff && fileDiff > 0;
+        }
+    } else {
+        return 0n;
+    }
+
+    let rays = HyperbolaQuintessenceAlgorithm(end, start, mask);
+    let ray = isPositiveRay ? rays[0] : rays[1];
+
+    if (includeStart) ray = ray | start;
+    if (!includeEnd) ray = ray & ~end;
+
+    return ray;
+}
+
+function GetNumberOfBits(bitboard) {
+    let numberOfBits = 0;
+    while (bitboard > 0n) {
+        bitboard = (bitboard - 1) & bitboard;
+        numberOfBits++;
+    }
+    return numberOfBits;
+}
+
+//****** change return value from array
+//Moves calculated using o^(o-2r) trick. 
+//Taken from https://www.youtube.com/watch?v=bCH4YK6oq8M&list=PLQV5mozTHmacMeRzJCW_8K3qw2miYqd0c&index=9&ab_channel=LogicCrazyChess.
+function HyperbolaQuintessenceAlgorithm(occupied, position, mask) {
+    let blockers = occupied & mask;
+    let positiveRay = ((blockers - 2n * position) ^ occupied) & mask;
+    let negativeRay = (Reverse((Reverse(blockers) - 2n * Reverse(position))) ^ occupied) & mask;
+    return [positiveRay, negativeRay];
+}
+
 function Reverse(bitboard) {
     let bitboardString = BitboardToString(bitboard);
 
