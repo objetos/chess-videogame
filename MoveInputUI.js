ec54f81596539ef04cab2279374563d864fee2af
David <jdiazga@unal.edu.co>
2024-04-17 15:36:03 -0500

    Add UI so available moves are highlighted when selecting a piece

--- MoveInputUI.js
+++ MoveInputUI.js
@@ -1,6 +1,8 @@
+//****** document class, test
 class MoveInputUI {
     #UIQuadrille;
     #colorForSelectedSquare = color(100, 100);
+    #colorForAvailableMoves = color(179, 255, 179);
     #moveCompleted = false;
 
     constructor() {
@@ -11,28 +13,36 @@ class MoveInputUI {
         MoveInput.addInputEventListener(MoveInput.E_InputEvents.MoveCanceled, this.#onMoveCanceled.bind(this));
     }
 
-    #onMoveStartSet(event) {
+    #onMoveStartSet(event, board) {
         if (this.#moveCompleted) {
             this.#Clear();
             this.#moveCompleted = false;
         }
-        this.#fillSquare(event.detail.square);
+        let square = event.detail.square;
+        this.#fillSquare(square.rank, square.file, this.#colorForSelectedSquare);
+        for (let move of legalMoves) {
+            if (move.startRank === square.rank && move.startFile === square.file) {
+                this.#fillSquare(move.endRank, move.endFile, this.#colorForAvailableMoves);
+            }
+        }
     }
 
     #onMoveDestinationSet(event) {
-        this.#fillSquare(event.detail.square);
+        this.#fillSquare(event.detail.square.rank, event.detail.square.file, this.#colorForSelectedSquare);
     }
 
-    #fillSquare(square) {
-        let row = 8 - square.rank;
-        let column = square.file - 1;
-        this.#UIQuadrille.fill(row, column, this.#colorForSelectedSquare);
+    #fillSquare(rank, file, color) {
+        let row = 8 - rank;
+        let column = file - 1;
+        this.#UIQuadrille.fill(row, column, color);
     }
 
     #onMoveInput(event) {
         let result = isMoveLegal(event.detail.move);
         if (!result.isLegal) {
             this.#Clear();
+        } else {
+            this.#UIQuadrille.replace(this.#colorForAvailableMoves, null);
         }
         this.#moveCompleted = true;
     }
@@ -42,7 +52,7 @@ class MoveInputUI {
     }
 
     #Clear() {
-        this.#UIQuadrille.replace(this.#colorForSelectedSquare, null);
+        this.#UIQuadrille.clear();
     }
 
     draw() {
